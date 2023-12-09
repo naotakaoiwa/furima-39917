0146d5d91f6c4da3e0adc191595cb4ab7fbf7b51
naotakaoiwa <babtos.0506@gmail.com>
2023-12-09 17:13:08 +0900

    単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name        {Faker::Lorem.sentence}
+    description      {Faker::Lorem.sentence}
+    category_id      {'2'}
+    condition_id     {'2'}
+    shipping_id      {'2'}
+    prefecture_id    {'2'}
+    delivery_time_id {'2'}
+    price            {Faker::Number.number(digits: 4)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
