158d97a71ac64c8ee5318c72beae12e0dd910d0e
naotakaoiwa <babtos.0506@gmail.com>
2023-12-15 21:41:49 +0900

    商品削除機能修正1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,10 +38,12 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    return unless item.user_id == current_user.id
-
-    item.destroy
-    redirect_to root_path
+    if item.user_id == current_user.id
+      item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
