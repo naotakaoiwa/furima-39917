d14852a2483a8057973b8797e093d9ff5be8ede6
naotakaoiwa <babtos.0506@gmail.com>
2023-12-20 20:12:54 +0900

    フォームの情報を保存出来るよう実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
     validates :user_id, :item_id, :city, :block
     validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid." }
-    validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   end
 
   def save
