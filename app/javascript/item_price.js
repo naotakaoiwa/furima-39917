7c9bf6afdafc4307ed8cb41e560dc276492c748b
naotakaoiwa <babtos.0506@gmail.com>
2023-12-24 14:21:11 +0900

    商品出品機能の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -7,4 +7,7 @@ window.addEventListener('turbo:load', () => {
     const addProfitDom = document.getElementById("profit")
     addProfitDom.innerHTML = inputValue - addTaxDom.innerHTML
   })
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
